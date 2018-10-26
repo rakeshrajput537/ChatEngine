e2f1b36e74785c20bfdddd00f55d34c4b6532659
rakesh kumar <rakeshrajput537@gmail.com>
2018-10-26 14:28:08 +0530

    username

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 user = [];
 connection = [];
-console.log('server running....');
+console.log('server running on 3000....');
 server.listen(process.env.PORT || 3000);
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
@@ -14,18 +14,18 @@ io.sockets.on('connection', function (socket) {
 	console.log('Connected: %s users connected', connection.length);
 
 	// Disconnect
-     socket.on('disconnect', function (data) {
-	users.splice(users.indexOf(socket.username), 1)
-	updateUsernames();
-	connection.splice(connection.indexOf(socket), 1);
-	console.log('Disconnected: %s users disconnected', connection.length);
-});
+ //     socket.on('disconnect', function (data) {
+	// users.splice(users.indexOf(socket.username), 1)
+	// updateUsernames();
+	// connection.splice(connection.indexOf(socket), 1);
+	// console.log('Disconnected: %s users disconnected', connection.length);
+// });
 // send message
 socket.on('send message', function(data){
 	console.log(data);
 	io.sockets.emit('new message',{msg: data});
 });
-new user
+
 socket.on('new user', function (data, callback) {
 	callback(true);
 	socket.username = data;
